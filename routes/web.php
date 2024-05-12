8252c473d5ddf70660974da4f93434aefaad67f2
Yannick <yannfscz@gmail.com>
2024-05-11 23:58:23 -0500

    feat: modified web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
+use App\Http\Controllers\CourseController;
+use App\Http\Controllers\DocentController;
+use App\Http\Controllers\PersonController;
+use App\Http\Controllers\StudentController;
+use App\Http\Controllers\TuitonController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::resource('courses', CourseController::class);
+// Route::resource('docents', DocentController::class);
+Route::resources([
+    'courses' => CourseController::class,
+    'docents' => DocentController::class,
+]);
+Route::resource('persons', PersonController::class);
+Route::resource('students', StudentController::class);
+Route::resource('tuitons', TuitonController::class);
